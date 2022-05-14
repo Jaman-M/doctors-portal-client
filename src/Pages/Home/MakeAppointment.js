ca7dad1ddc167436e044501a1b88eaea010b9c20
Jaman-M <jaman.manik1@gmail.comm>
2022-05-15 00:40:11 +0600

    create Appointment Banner using React Day Picker

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -13,10 +13,10 @@ const MakeAppointment = () => {
             <div className='flex-1 hidden lg:block'>
                 <img className='mt-[-100px]' src={doctor} alt="" />
             </div>
-            <div className='flex-1'>
+            <div className='flex-1 px-5 py-5'>
                 <h3 className='text-xl text-primary font-bold'>Appointment</h3>
-                <h2 className='text-3xl text-white'>Make an Appointment</h2>
-                <p className='text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam molestiae, tempore eveniet natus aut possimus quos corrupti! Assumenda vero iure expedita delectus numquam eaque fugit sapiente, eum iusto, velit recusandae repellat incidunt magni provident corrupti sed laboriosam sint. Obcaecati, officiis!</p>
+                <h2 className='text-3xl text-white py-5'>Make an Appointment</h2>
+                <p className='text-white pb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam molestiae, tempore eveniet natus aut possimus quos corrupti! Assumenda vero iure expedita delectus numquam eaque fugit sapiente, eum iusto, velit recusandae repellat incidunt magni provident corrupti sed laboriosam sint. Obcaecati, officiis!</p>
                 <PrimaryButton>Get Started</PrimaryButton>
             </div>
         </section>
