b596a1e76a14049bb48350cb8c23058ead022491
Jaman-M <jaman.manik1@gmail.comm>
2022-05-14 19:59:57 +0600

    improved testimonials

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Review = (review) => {
+const Review = ({review}) => {
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
   <div class="card-body">
     
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In fuga nulla itaque aut hic soluta!</p>
-    <div class="flex">
+    <div class="flex items-center">
     <div class="avatar">
-  <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+  <div class="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
     <img src={review.img} alt="" />
   </div>
 </div>
--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import quote from '../../assets/icons/quote.svg';
-import people1 from '../../assets/images/people1.png'
-import people2 from '../../assets/images/people2.png'
-import people3 from '../../assets/images/people3.png'
+import people1 from '../../assets/images/people1.png';
+import people2 from '../../assets/images/people2.png';
+import people3 from '../../assets/images/people3.png';
 import Review from './Review';
 
 const Testimonials = () => {
@@ -24,7 +24,7 @@ const Testimonials = () => {
             name: 'Winson Herry',
             location:'california',
             img: people3
-        },
+        }
     ];
     return (
         <section className='my-20'>
