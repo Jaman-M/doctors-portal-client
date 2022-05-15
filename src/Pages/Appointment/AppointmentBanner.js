5abadfa9b726ae329cf691768a596e565cd86dd8
Jaman-M <jaman.manik1@gmail.comm>
2022-05-15 16:11:58 +0600

    booking options and available slots with conditional rendering

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import chair from '../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
-import { format } from 'date-fns';
+// import { format } from 'date-fns';
+import bg from '../../assets/images/bg.png'
 
-const AppointmentBanner = () => {
-    const [date, setDate] = useState(new Date());
+const AppointmentBanner = ({date, setDate}) => {
+    
     return (
-        <div class="hero min-h-screen ">
+        <div
+        style={{
+            background: `url(${bg})`,
+            backgroundSize:'cover'
+        }}
+         class="hero min-h-screen ">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div>
@@ -16,7 +22,7 @@ const AppointmentBanner = () => {
                         selected={date}
                         onSelect={setDate}
                     />
-                    <p>You have selected: {format(date, 'PP')}.</p>
+                    
                 </div>
             </div>
         </div>
